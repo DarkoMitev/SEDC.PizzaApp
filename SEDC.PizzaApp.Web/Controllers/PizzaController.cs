bf979845deb434b2b3bbf3fdfb8205d2230e8ef6
damjan03 <jakimov.damjan@yahoo.com>
2023-06-12 20:45:54 +0200

    Add project files.

---
+++ SEDC.PizzaApp.Web/Controllers/PizzaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Web.Data;
+using SEDC.PizzaApp.Web.Mapper;
+using SEDC.PizzaApp.Web.Models.ViewModels;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SEDC.PizzaApp.Web.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            //var pizzas = PizzaAppDb.Pizzas.Select(x => new PizzaViewModel
+            //{
+            //    Id = x.Id,
+            //    Name = x.Name,
+            //    Price = x.Price,
+            //    IsOnPromotion = x.IsOnPromotion
+
+            var viewModels = PizzaAppDb.Pizzas.Select(x => x.ToViewModel()).ToList();
+            return View(viewModels);
+        }
+    }
+}
---
+++ SEDC.PizzaApp.Web/Mapper/PizzaMapper.cs
@@ -0,0 +1,21 @@
+﻿using SEDC.PizzaApp.Web.Models.Domain;
+using SEDC.PizzaApp.Web.Models.ViewModels;
+using System.Runtime.CompilerServices;
+
+namespace SEDC.PizzaApp.Web.Mapper
+{
+    public static class PizzaMapper
+    {
+        public static PizzaViewModel ToViewModel(this Pizza pizza)
+        {
+            return new PizzaViewModel
+            {
+                Id = pizza.Id,
+                Name = pizza.Name,
+                IsOnPromotion = pizza.IsOnPromotion,
+                Price = pizza.Price
+            };
+        }
+
+    }
+}
